c4676222e320a26fb68748f494e3a8790fa666bb
Brett Longworth <blongworth@whoi.edu>
2024-08-13 14:38:09 +0000

    Remove extra files

--- fluorologger/db.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-def write_to_database(timestamp, gain, voltage, concentration):
-    c.execute("INSERT INTO data VALUES (?, ?, ?, ?)", (timestamp, gain, voltage, concentration))
-    conn.commit()
--- fluorologger/pynmeagps-test.py
+++
@@ -1,9 +0,0 @@
-from serial import Serial
-from pynmeagps import NMEAReader
-with Serial('COM3', 9600, timeout=3) as stream:
-    nmr = NMEAReader(stream)
-    while True:
-        raw_data, parsed_data = nmr.read()
-        if parsed_data is not None and parsed_data.msgID == 'GGA':
-            print(parsed_data)
-            print(f"time: {parsed_data.time} lat: {parsed_data.lat} lon: {parsed_data.lon}")
